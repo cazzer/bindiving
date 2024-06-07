e2dfba9fb8b6f394db441c1a5008456cca4fd82d
Caleb Brewer <caleb@cazzer.io>
2024-06-06 22:45:27 -0400

    cleaning up and formatting

--- components/header.jsx
+++ components/header.jsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 
-import logo from 'public/images/bin-diving.svg'
+import logo from 'public/images/bin-diving-white.svg'
 
 const navItems = [{ linkText: 'Home', href: '/' }]
 
